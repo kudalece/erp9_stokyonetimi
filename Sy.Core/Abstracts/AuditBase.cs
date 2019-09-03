ab01fb570ff5a028f883e5fc9241ed562fd317a0
Ece <kudalece@gmail.com>
2019-09-03 10:49:14 +0300

    .

---
+++ Sy.Core/Abstracts/AuditBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sy.Core.Abstracts
+{
+
+  
+   public abstract class AuditBase
+    {
+
+       
+
+
+        [StringLength(50)]
+        public string CreatedUser { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        [StringLength(50)]
+        public string UpdateUser { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+
+    }
+}
---
+++ Sy.Core/Abstracts/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sy.Core.Abstracts
+{
+    public abstract class BaseEntity<TKey> : IEntity<TKey>
+    {
+        [Key]
+        public TKey Id { get; set; }
+
+
+    }
+}
